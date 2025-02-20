4a0576485499d6e08f96cc35592609535c5b507b
Rory <najar1412@users.noreply.github.com>
2025-02-20 15:12:06 +0000

    add wp engine as image host

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const { withFaust, getWpHostname } = require("@faustwp/core");
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust({ reactStrictMode: true, swcMinify: true });
+module.exports = withFaust({
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bpheadlessb541.wpenginepowered.com",
+      },
+    ],
+  },
+});
