74690a48cb2515e4e13dcd2a18a5a241604e25b1
Rory <najar1412@users.noreply.github.com>
2025-03-03 11:45:45 +0000

    user feedback

--- src/components/forms/GetInTouchForm.jsx
+++ src/components/forms/GetInTouchForm.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 import { useForm } from "react-hook-form";
-import { Button, Stack } from "@mantine/core";
+import { Button, Divider, Stack, Text } from "@mantine/core";
 
 import styles from "./GetInTouchForm.module.css";
 
 export const GetInTouchForm = () => {
+  const [submitted, setSubmitted] = useState(false);
   const {
     register,
     handleSubmit,
@@ -11,46 +14,60 @@ export const GetInTouchForm = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    setSubmitted(true);
+    console.log(data);
+  };
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input
-          className={styles.input}
-          placeholder="name"
-          {...register("name")}
-        />
+    <>
+      {submitted ? (
+        <Stack justify="center" h={"100%"}>
+          <Divider hiddenFrom="md" />
+          <Text c="white" fw="bold">
+            Thank you, a member of our team will be in touch!
+          </Text>
+        </Stack>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack>
+            {/* register your input into the hook by invoking the "register" function */}
+            <input
+              className={styles.input}
+              placeholder="name"
+              {...register("name")}
+            />
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <input
-          className={styles.input}
-          placeholder="company"
-          {...register("company")}
-        />
-        <input
-          className={styles.input}
-          placeholder="email"
-          {...register("email")}
-        />
+            {/* include validation with required or other standard HTML validation rules */}
+            <input
+              className={styles.input}
+              placeholder="company"
+              {...register("company")}
+            />
+            <input
+              className={styles.input}
+              placeholder="email"
+              {...register("email")}
+            />
 
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+            {/* errors will return when field validation fails  */}
+            {errors.exampleRequired && <span>This field is required</span>}
 
-        <Button
-          ml="auto"
-          px="xl"
-          color={"brand.2"}
-          c={"brand.0"}
-          maw={"fit-content"}
-          radius={"2rem"}
-          type="submit"
-        >
-          Contact
-        </Button>
-      </Stack>
-    </form>
+            <Button
+              ml="auto"
+              px="xl"
+              color={"brand.2"}
+              c={"brand.0"}
+              maw={"fit-content"}
+              radius={"2rem"}
+              type="submit"
+            >
+              Contact
+            </Button>
+          </Stack>
+        </form>
+      )}
+    </>
   );
 };
--- src/components/forms/NewsletterForm.jsx
+++ src/components/forms/NewsletterForm.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 import { useForm } from "react-hook-form";
-import { Stack, UnstyledButton, Text } from "@mantine/core";
+import { Stack, UnstyledButton, Text, Divider } from "@mantine/core";
 import Image from "next/image";
 
 import styles from "./NewsletterForm.module.css";
@@ -7,6 +9,7 @@ import styles from "./NewsletterForm.module.css";
 import arrow from "../../assets/arrow-tr-blue.svg";
 
 export const NewsletterForm = () => {
+  const [submitted, setSubmitted] = useState(false);
   const {
     register,
     handleSubmit,
@@ -14,30 +17,48 @@ export const NewsletterForm = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    setSubmitted(true);
+    console.log(data);
+  };
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
-      <Stack mt="1rem">
-        <div className={styles["input-container"]}>
-          <UnstyledButton type="submit">
-            <Image alt="arrow" src={arrow} className={styles.arrow} />
-          </UnstyledButton>
-          <input
-            className={styles.input}
-            placeholder="Your email"
-            {...register("email", { required: true })}
-          />
-        </div>
 
-        {/* errors will return when field validation fails  */}
-        {errors.email && (
-          <Text size="xs" c={"red"}>
-            An Email address is required
-          </Text>
-        )}
+    submitted ? (
+      <Stack w={"100%"}>
+        <Text
+          fw={"bold"}
+          size="xs"
+          c="brand.0"
+          mt="2rem"
+          ta={{ base: "center", xs: "left" }}
+        >
+          Thank you!
+        </Text>
       </Stack>
-    </form>
+    ) : (
+      <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
+        <Stack mt="1rem">
+          <div className={styles["input-container"]}>
+            <UnstyledButton type="submit">
+              <Image alt="arrow" src={arrow} className={styles.arrow} />
+            </UnstyledButton>
+            <input
+              className={styles.input}
+              placeholder="Your email"
+              {...register("email", { required: true })}
+            />
+          </div>
+
+          {/* errors will return when field validation fails  */}
+          {errors.email && (
+            <Text size="xs" c={"red"}>
+              An Email address is required
+            </Text>
+          )}
+        </Stack>
+      </form>
+    )
   );
 };
