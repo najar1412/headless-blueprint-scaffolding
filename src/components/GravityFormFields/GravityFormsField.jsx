fc31377f41c7b0415a2fedc651b5cb51d4fac5c6
Rory <najar1412@users.noreply.github.com>
2025-03-20 14:52:34 +0000

    remove name field

--- src/components/GravityFormFields/GravityFormsField.jsx
+++ src/components/GravityFormFields/GravityFormsField.jsx
@@ -1,16 +1,11 @@
 import EmailField from "./fields/EmailField";
 import TextField from "./fields/TextField";
-import NameField from "./fields/NameField";
 
 export default function GravityFormsField({ formId, field, fieldErrors }) {
   switch (field.type) {
     case "EMAIL":
       return (
         <EmailField formId={formId} field={field} fieldErrors={fieldErrors} />
-      );
-    case "NAME":
-      return (
-        <NameField formId={formId} field={field} fieldErrors={fieldErrors} />
       );
     case "TEXT":
       return (
